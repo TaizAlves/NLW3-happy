c6da22d069b50c8a66bdb6c934c2d60d627a6b67
Taiz Alves <taizralves@gmail.com>
2020-10-14 16:42:34 -0300

    initial commit

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express';
+
+import { getRepository } from 'typeorm';
+import Orphanages from '../models/Orphanages';
+
+export default {
+
+    async index(request: Request, response: Response) {
+        const orphanagesRepository = getRepository(Orphanages);
+
+        const orphanages = await orphanagesRepository.find();
+
+        return response.json(orphanages);
+
+    },
+
+    async create(request: Request, response: Response) {
+        const {
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+          } = request.body
+          
+          const orphanagesRepository = getRepository(Orphanages);
+        
+          const orphanage = orphanagesRepository.create({
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+          });
+      
+          await orphanagesRepository.save(orphanage);
+      
+          return response.status(201).json(orphanage);
+    }
+}
